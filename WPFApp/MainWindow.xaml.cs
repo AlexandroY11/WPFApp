2b05e614a149cadaaa3a9c32f3a353d1f68c33b4
Alexandro Yule <alexandroyule@gmail.com>
2024-12-13 20:41:22 -0500

    Ejecutando Taask con metodo ShowMessage

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFApp
 {
@@ -20,5 +11,19 @@ namespace WPFApp
         {
             InitializeComponent();
         }
+
+        void CreateTask()
+        {
+            Task T;
+            Action Code = new Action(ShowMessage);
+            T = new Task(Code);
+
+        }
+
+        void ShowMessage()
+        {
+            MessageBox.Show("Ejecutando el método ShowMessage");
+        }
+
     }
 }
